d85651a6941c664494ffbf67d4c088cae3264460
dsokolovskiy <souldead147@gmail.com>
2024-07-22 17:04:33 +0300

    create forms for Dish, DishType & Cook

---
+++ kitchen/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Dish, DishType, Cook
+from django.contrib.auth.forms import UserCreationForm
+
+
+class DishForm(forms.ModelForm):
+    class Meta:
+        model = Dish
+        fields = ["name", "description", "price", "dish_type", "cooks"]
+
+
+class DishTypeForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = ["name"]
+
+
+class CookForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["username", "email", "password1", "password2", "first_name", "last_name", "years_of_experience"]
