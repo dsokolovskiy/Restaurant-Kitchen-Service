2359629193b5f9cae82fb96ecfa58bb27ed395c7
dsokolovskiy <souldead147@gmail.com>
2024-07-23 20:31:03 +0300

    modified views.py & add new file dish_delete in templates/kitchen

--- kitchen/views.py
+++ kitchen/views.py
@@ -3,20 +3,24 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Dish, DishType, Cook
 from .forms import DishForm, DishTypeForm, CookForm
 
+
 class HomePageView(ListView):
     model = Dish
     template_name = 'kitchen/home.html'
     context_object_name = 'dishes'
+
 
 class DishListView(ListView):
     model = Dish
     template_name = 'kitchen/dish_list.html'
     context_object_name = 'dish_list'
 
+
 class DishDetailView(DetailView):
     model = Dish
     template_name = 'kitchen/dish_detail.html'
     context_object_name = 'dish'
+
 
 class DishCreateView(CreateView):
     model = Dish
@@ -24,26 +28,31 @@ class DishCreateView(CreateView):
     template_name = 'kitchen/dish_create.html'
     success_url = reverse_lazy('kitchen:dish_list')
 
+
 class DishUpdateView(UpdateView):
     model = Dish
     form_class = DishForm
     template_name = 'kitchen/dish_update.html'
     success_url = reverse_lazy('kitchen:dish_list')
 
+
 class DishDeleteView(DeleteView):
     model = Dish
     template_name = 'kitchen/dish_delete.html'
     success_url = reverse_lazy('kitchen:dish_list')
+
 
 class DishTypeListView(ListView):
     model = DishType
     template_name = 'kitchen/dishtype_list.html'
     context_object_name = 'dishtype_list'
 
+
 class DishTypeDetailView(DetailView):
     model = DishType
     template_name = 'kitchen/dishtype_detail.html'
     context_object_name = 'dishtype'
+
 
 class DishTypeCreateView(CreateView):
     model = DishType
@@ -51,26 +60,31 @@ class DishTypeCreateView(CreateView):
     template_name = 'kitchen/dishtype_create.html'
     success_url = reverse_lazy('kitchen:dishtype_list')
 
+
 class DishTypeUpdateView(UpdateView):
     model = DishType
     form_class = DishTypeForm
     template_name = 'kitchen/dishtype_update.html'
     success_url = reverse_lazy('kitchen:dishtype_list')
 
+
 class DishTypeDeleteView(DeleteView):
     model = DishType
     template_name = 'kitchen/dishtype_delete.html'
     success_url = reverse_lazy('kitchen:dishtype_list')
+
 
 class CookListView(ListView):
     model = Cook
     template_name = 'kitchen/cook_list.html'
     context_object_name = 'cook_list'
 
+
 class CookDetailView(DetailView):
     model = Cook
     template_name = 'kitchen/cook_detail.html'
     context_object_name = 'cook'
+
 
 class CookCreateView(CreateView):
     model = Cook
@@ -78,11 +92,13 @@ class CookCreateView(CreateView):
     template_name = 'kitchen/cook_create.html'
     success_url = reverse_lazy('kitchen:cook_list')
 
+
 class CookUpdateView(UpdateView):
     model = Cook
     form_class = CookForm
     template_name = 'kitchen/cook_update.html'
     success_url = reverse_lazy('kitchen:cook_list')
+
 
 class CookDeleteView(DeleteView):
     model = Cook
