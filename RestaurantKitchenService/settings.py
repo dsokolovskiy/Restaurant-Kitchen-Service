63ae139e2dc920733822eac94c5993bdbe8ce5ff
dsokolovskiy <souldead147@gmail.com>
2024-07-23 15:25:39 +0300

    delete debug-toolbar

--- RestaurantKitchenService/settings.py
+++ RestaurantKitchenService/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kitchen',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'RestaurantKitchenService.urls'
@@ -126,7 +124,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
