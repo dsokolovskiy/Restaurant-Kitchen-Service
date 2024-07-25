27f1cab3180276eefe37b20dd38020aaec8134eb
dsokolovskiy <souldead147@gmail.com>
2024-07-25 19:43:14 +0300

    update test for test_views.py

--- kitchen/tests/test_views.py
+++ kitchen/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from kitchen.models import Dish, DishType, Cook
+
 from kitchen.forms import DishForm, DishTypeForm, CookForm
 
 
