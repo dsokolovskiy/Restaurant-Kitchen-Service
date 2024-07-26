5bab70a6b15833b5456e4f57e3ddad394565f538
dsokolovskiy <souldead147@gmail.com>
2024-07-26 12:22:24 +0300

    fix flake8 in admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Dish, DishType, Cook
 
+
 class DishAdmin(admin.ModelAdmin):
     list_display = ('name', 'dish_type', 'get_cooks')
     search_fields = ('name',)
@@ -10,13 +11,17 @@ class DishAdmin(admin.ModelAdmin):
         return ", ".join([cook.username for cook in obj.cooks.all()])
     get_cooks.short_description = 'Cooks'
 
+
 class DishTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
     search_fields = ('name',)
+
 
 class CookAdmin(admin.ModelAdmin):
     list_display = ('username', 'years_of_experience')
     search_fields = ('username',)
+
 
 admin.site.register(Dish, DishAdmin)
 admin.site.register(DishType, DishTypeAdmin)
