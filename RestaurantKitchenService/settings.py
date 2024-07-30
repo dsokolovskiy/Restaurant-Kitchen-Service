db4320e0aa64ee19f8328b2c684154ade3b16ac1
dsokolovskiy <souldead147@gmail.com>
2024-07-30 19:41:21 +0300

    add restaurant-kitchen-service-2.onrender.com to ALLOWED_HOSTS

--- RestaurantKitchenService/settings.py
+++ RestaurantKitchenService/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-2.onrender.com"]
 
 
 # Application definition
